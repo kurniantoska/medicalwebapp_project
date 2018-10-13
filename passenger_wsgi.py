7aa40727042e73bf229557edac98843d3a30eb94
kurniantoska <alternative.xen.sakura@gmail.com>
2018-10-14 06:48:01 +0700

    update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,12 +5,12 @@ import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-# wsgi = imp.load_source('wsgi', 'medisproj/wsgi.py')
-# application = wsgi.application
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'Maintenance... \n'
-    # version = 'Python %s\n' % sys.version.split()[0]
-    version = 'Sedang dalam pengembangan...!'
-    response = '\n'.join([message, version])
-    return [response.encode()]
+wsgi = imp.load_source('wsgi', 'medisproj/wsgi.py')
+application = wsgi.application
+# def application(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'Maintenance... \n'
+#     # version = 'Python %s\n' % sys.version.split()[0]
+#     version = 'Sedang dalam pengembangan...!'
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
