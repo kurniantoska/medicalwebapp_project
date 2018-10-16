b6b28c27f3b8a9ef56f00f02560f0268bac6c66c
kurniantoska <alternative.xen.sakura@gmail.com>
2018-10-16 18:40:54 +0700

    wanna fix lingkar perut

--- medisproj/urls.py
+++ medisproj/urls.py
@@ -29,6 +29,9 @@ urlpatterns = [
     path('', views.home_page, name='home'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
